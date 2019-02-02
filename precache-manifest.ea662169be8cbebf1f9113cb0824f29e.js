95db1474ed01b9d26abb5b62a18d9141c8e61147
Shaun <slewin419@gmail.com>
2019-02-02 15:44:12 -0500

    Updates

---
+++ precache-manifest.ea662169be8cbebf1f9113cb0824f29e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66aa13528afdd41dcd4e",
+    "url": "/periodic-table-react/static/js/runtime~main.66aa1352.js"
+  },
+  {
+    "revision": "135b0c8b8c33146e8fcd",
+    "url": "/periodic-table-react/static/js/main.135b0c8b.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/periodic-table-react/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "135b0c8b8c33146e8fcd",
+    "url": "/periodic-table-react/static/css/main.6f6bee03.chunk.css"
+  },
+  {
+    "revision": "55a8dc16ddb9966473dc6450da6aefdc",
+    "url": "/periodic-table-react/index.html"
+  }
+];
